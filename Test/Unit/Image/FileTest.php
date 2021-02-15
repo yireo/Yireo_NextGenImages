63410cb7887da38fad963c28323485556d355162
jisse Reitsma <jisse@yireo.com>
2021-02-15 14:42:52 +0100

    Fix unit test

--- Test/Unit/Image/FileTest.php
+++ Test/Unit/Image/FileTest.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Yireo\NextGenImages\Test\Unit\Image;
 
@@ -8,6 +7,7 @@ use Magento\Framework\Filesystem\Directory\ReadFactory as DirectoryReadFactory;
 use Magento\Framework\Filesystem\Driver\File as FileDriver;
 use Yireo\NextGenImages\Image\File;
 use PHPUnit\Framework\TestCase;
+use Yireo\NextGenImages\Logger\Debugger;
 
 /**
  * Class FileTest testing behaviour of File
@@ -41,7 +41,11 @@ class FileTest extends TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $target = new File($directoryListMock, $directoryReadFactoryMock, $fileDriverMock); // phpstan:ignore
+        $debugger = $this->getMockBuilder(Debugger::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $target = new File($directoryListMock, $directoryReadFactoryMock, $fileDriverMock, $debugger); // phpstan:ignore
         return $target;
     }
 }
