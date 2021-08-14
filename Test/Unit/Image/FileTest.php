46e09a58b07f69d98e6c16287f5eb1d0df853376
Jisse Reitsma <jisse@yireo.com>
2021-08-14 17:37:44 +0200

    Add various unit tests

--- Test/Unit/Image/FileTest.php
+++ Test/Unit/Image/FileTest.php
@@ -2,12 +2,13 @@
 
 namespace Yireo\NextGenImages\Test\Unit\Image;
 
+use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Filesystem\DirectoryList;
-use Magento\Framework\Filesystem\Directory\ReadFactory as DirectoryReadFactory;
 use Magento\Framework\Filesystem\File\ReadFactory as FileReadFactory;
 use Magento\Framework\Filesystem\Driver\File as FileDriver;
 use Magento\Framework\Filesystem\File\ReadInterface;
 use Yireo\NextGenImages\Image\File;
+use Yireo\NextGenImages\Image\UrlConvertor;
 use PHPUnit\Framework\TestCase;
 use Yireo\NextGenImages\Logger\Debugger;
 
@@ -17,46 +18,336 @@ use Yireo\NextGenImages\Logger\Debugger;
 class FileTest extends TestCase
 {
     /**
-     * @test Test the resolve() function
+     * @test \Yireo\NextGenImages\Image\File::resolve()
      */
     public function testResolve(): void
     {
-        $target = $this->getTarget();
-        $resolvedFile = $target->resolve('http://anotherhost.com/some/fake/url.png');
-        $this->assertSame($resolvedFile, '/pub/some/fake/url.png');
+        $urlConvertor = $this->getUrlConvertorMock();
+        $urlConvertor->method('getFilenameFromUrl')->willReturn('/pub/some/fake/url.png');
+        $fileRead = $this->getFileReadMockWithoutExistingFile();
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $this->getFileDriverMock(),
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMockWithNonExistingFiles(),
+            $urlConvertor
+        ); // phpstan:ignore
+
+        $resolvedFile = $file->resolve('http://anotherhost.com/some/fake/url.png');
+        $this->assertSame('/pub/some/fake/url.png', $resolvedFile);
     }
 
     /**
-     * @return File
+     * @test \Yireo\NextGenImages\Image\File::fileExists()
      */
-    public function getTarget(): File
+    public function testFileExistsWithExistingFile(): void
     {
-        $directoryListMock = $this->getMockBuilder(DirectoryList::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $fileDriverMock = $this->getMockBuilder(FileDriver::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $debugger = $this->getMockBuilder(Debugger::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $fileReadFactoryMock = $this->getMockBuilder(FileReadFactory::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $fileReadMock = $this->getMockBuilder(ReadInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $fileReadFactoryMock->method('create')->willReturn($fileReadMock);
-
-        return new File(
-            $directoryListMock,
-            $fileDriverMock,
-            $debugger,
-            $fileReadFactoryMock
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $this->getFileDriverMock(),
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMockWithExistingFiles(),
+            $this->getUrlConvertorMock()
         ); // phpstan:ignore
+
+        $this->assertSame(true, $file->fileExists('/some/fake/url.png'));
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::fileExists()
+     */
+    public function testFileExistsWithoutExistingFile(): void
+    {
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $this->getFileDriverMock(),
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMockWithNonExistingFiles(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+
+        $this->assertSame(false, $file->fileExists('/some/fake/url.png'));
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::convertSuffix()
+     */
+    public function testConvertSuffix()
+    {
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $this->getFileDriverMock(),
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMock(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+
+        $destination = $file->convertSuffix('/some/path/example.jpg', '.webp');
+        $this->assertSame('/some/path/example.webp', $destination);
+
+        $destination = $file->convertSuffix('/some/path/example.JPG', '.webp');
+        $this->assertSame('/some/path/example.webp', $destination);
+
+        $destination = $file->convertSuffix('/some/path/example.jpeg', '.webp');
+        $this->assertSame('/some/path/example.webp', $destination);
+
+        $destination = $file->convertSuffix('/some/path/example.png', '.webp');
+        $this->assertSame('/some/path/example.webp', $destination);
+
+        $destination = $file->convertSuffix('/some/path/example.jpeg', '.foobar');
+        $this->assertSame('/some/path/example.foobar', $destination);
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::getModificationTime()
+     */
+    public function testGetModificationTime()
+    {
+        $now = time();
+        $fileDriverMock = $this->getFileDriverMock();
+        $fileDriverMock->method('stat')->willReturn(['mtime' => $now]);
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $fileDriverMock,
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMock(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+        $this->assertSame($now, $file->getModificationTime('/foo/bar.png'));
+
+        $fileDriverMock = $this->getFileDriverMock();
+        $fileDriverMock->method('stat')->willReturn(['mtime' => $now]);
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $fileDriverMock,
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMock(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+        $this->assertSame($now, $file->getModificationTime('/foo/bar.png'));
+
+        $fileDriverMock = $this->getFileDriverMock();
+        $fileDriverMock->method('stat')->willReturn([]);
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $this->getFileDriverMock([]),
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMock(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+        $this->assertSame(0, $file->getModificationTime('/foo/bar.png'));
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::isNewerThan()
+     */
+    public function testIsNewerThanReturnsFalseWithFilesThatDontExist()
+    {
+        $now = time();
+        $fileDriverMock = $this->getFileDriverMock();
+        $fileDriverMock->method('stat')->willReturnMap([
+            ['target.png', ['ctime' => $now]],
+            ['source.png', ['ctime' => $now]]
+        ]);
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $fileDriverMock,
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMockWithExistingFiles(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+
+        $this->assertFalse($file->isNewerThan('target.png', 'source.png'));
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::isNewerThan()
+     */
+    public function testIsNewerThanReturnsFalseWithFilesCreatedAtSameTime()
+    {
+        $now = time();
+        $fileDriverMock = $this->getFileDriverMock();
+        $fileDriverMock->method('stat')->willReturnMap([
+            ['target.png', ['ctime' => $now]],
+            ['source.png', ['ctime' => $now]]
+        ]);
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $fileDriverMock,
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMockWithExistingFiles(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+
+        $this->assertFalse($file->isNewerThan('target.png', 'source.png'));
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::isNewerThan()
+     */
+    public function testIsNewerThanReturnsFalseWithTargetFileNewerThanSource()
+    {
+        $now = time();
+        $fileDriverMock = $this->getFileDriverMock();
+        $fileDriverMock->method('stat')->willReturnMap([
+            ['target.png', ['ctime' => $now + 1000]],
+            ['source.png', ['ctime' => $now]]
+        ]);
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $fileDriverMock,
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMockWithExistingFiles(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+
+        $this->assertTrue($file->isNewerThan('target.png', 'source.png'));
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::needsConversion()
+     */
+    public function testNeedsConversionWithExistingFiles()
+    {
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $this->getFileDriverMock(),
+            $this->getDebuggerMock(),
+            $this->getFileReadFactoryMockWithExistingFiles(),
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+
+        $this->assertFalse($file->needsConversion('source.png', 'destination.webp'));
+    }
+
+    /**
+     * @test \Yireo\NextGenImages\Image\File::needsConversion()
+     */
+    public function testNeedsConversionWithNonExistingDestinationFile()
+    {
+        $fileReadFactoryMock = $this->getFileReadFactoryMock();
+        $fileReadFactoryMock->method('create')->willReturnMap([
+            ['source.png', 'file', $this->getFileReadMockWithExistingFile()],
+            ['destination.webp', 'file', $this->getFileReadMockWithoutExistingFile()]
+        ]);
+
+        $file = new File(
+            $this->getDirectoryListMock(),
+            $this->getFileDriverMock(),
+            $this->getDebuggerMock(),
+            $fileReadFactoryMock,
+            $this->getUrlConvertorMock()
+        ); // phpstan:ignore
+
+        $this->assertTrue($file->needsConversion('source.png', 'destination.webp'));
+    }
+
+    /**
+     * @return UrlConvertor
+     */
+    private function getUrlConvertorMock(): UrlConvertor
+    {
+        return $this->getMockBuilder(UrlConvertor::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @return FileReadFactory
+     */
+    private function getFileReadFactoryMockWithExistingFiles(): FileReadFactory
+    {
+        $fileRead = $this->getFileReadMockWithExistingFile();
+        $fileReadFactoryMock = $this->getFileReadFactoryMock();
+        $fileReadFactoryMock->method('create')->willReturn($fileRead);
+        return $fileReadFactoryMock;
+    }
+
+    /**
+     * @return FileReadFactory
+     */
+    private function getFileReadFactoryMockWithNonExistingFiles(): FileReadFactory
+    {
+        $fileRead = $this->getFileReadMockWithoutExistingFile();
+        $fileReadFactoryMock = $this->getFileReadFactoryMock();
+        $fileReadFactoryMock->method('create')->willReturn($fileRead);
+        return $fileReadFactoryMock;
+    }
+
+    /**
+     * @return FileReadFactory
+     */
+    private function getFileReadFactoryMock(): FileReadFactory
+    {
+        return $this->getMockBuilder(FileReadFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @return ReadInterface
+     */
+    private function getFileReadMockWithExistingFile(): ReadInterface
+    {
+        $fileReadMock = $this->getFileReadMock();
+        $fileReadMock->method('readAll')->willReturn('foobar');
+        return $fileReadMock;
+    }
+
+    /**
+     * @return ReadInterface
+     */
+    private function getFileReadMockWithoutExistingFile(): ReadInterface
+    {
+        $fileReadMock = $this->getFileReadMock();
+        $fileReadMock->method('readAll')->willThrowException(new FileSystemException(__('Nope')));
+        return $fileReadMock;
+    }
+
+    /**
+     * @return ReadInterface
+     */
+    private function getFileReadMock(): ReadInterface
+    {
+        return $this->getMockBuilder(ReadInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @return Debugger
+     */
+    private function getDebuggerMock(): Debugger
+    {
+        return $this->getMockBuilder(Debugger::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @return FileDriver
+     */
+    private function getFileDriverMock(): FileDriver
+    {
+        return $this->getMockBuilder(FileDriver::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @return DirectoryList
+     */
+    private function getDirectoryListMock(): DirectoryList
+    {
+        return $this->getMockBuilder(DirectoryList::class)
+            ->disableOriginalConstructor()
+            ->getMock();
     }
 }
---
+++ Test/Unit/Image/UrlConvertorTest.php
@@ -0,0 +1,96 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\NextGenImages\Test\Unit\Image;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Filesystem\DirectoryList;
+use Magento\Framework\UrlInterface;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Yireo\NextGenImages\Image\UrlConvertor;
+
+class UrlConvertorTest extends TestCase
+{
+    /**
+     * @test \Yireo\NextGenImages\Image\UrlConvertor::isLocal()
+     * @throws NoSuchEntityException
+     */
+    public function testIsLocal()
+    {
+        $urlConvertor = new UrlConvertor(
+            $this->getUrlMock('http://localhost/'),
+            $this->getStoreManagerMock('http://cdn/'),
+            $this->getDirectoryListMock()
+        );
+
+        $this->assertTrue($urlConvertor->isLocal('/media/test.png'));
+        $this->assertTrue($urlConvertor->isLocal('http://localhost/media/test.png'));
+        $this->assertTrue($urlConvertor->isLocal('http://cdn/test.png'));
+        $this->assertFalse($urlConvertor->isLocal('http://foobar/media/test.png'));
+    }
+
+    /**
+     * \Yireo\NextGenImages\Image\UrlConvertor::getFilenameFromUrl()
+     */
+    public function testGetFilenameFromUrl()
+    {
+        $urlConvertor = new UrlConvertor(
+            $this->getUrlMock('http://localhost/'),
+            $this->getStoreManagerMock('http://cdn/'),
+            $this->getDirectoryListMock()
+        );
+
+        $this->assertSame('/var/www/html/pub/media/sample.png', $urlConvertor->getFilenameFromUrl('http://cdn/sample.png'));
+        $this->assertSame('/var/www/html/pub/sample.webp', $urlConvertor->getFilenameFromUrl('http://localhost/sample.webp'));
+        $this->assertSame('/var/www/html/pub/static/sample.jpg', $urlConvertor->getFilenameFromUrl('http://localhost/static/version432423423/sample.jpg'));
+    }
+
+    /**
+     * @param string $baseUrl
+     * @return UrlInterface
+     */
+    private function getUrlMock(string $baseUrl): UrlInterface
+    {
+        $urlMock = $this->getMockBuilder(UrlInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $urlMock->method('getBaseUrl')->willReturn($baseUrl);
+        return $urlMock;
+    }
+
+    /**
+     * @param string $mediaBaseUrl
+     * @return StoreManagerInterface
+     */
+    private function getStoreManagerMock(string $mediaBaseUrl): StoreManagerInterface
+    {
+        $storeMock = $this->getMockBuilder(Store::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $storeMock->method('getBaseUrl')->willReturn($mediaBaseUrl);
+
+        $storeManagerMock = $this->getMockBuilder(StoreManagerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $storeManagerMock->method('getStore')->willReturn($storeMock);
+        return $storeManagerMock;
+    }
+
+    /**
+     * @return DirectoryList
+     */
+    private function getDirectoryListMock(): DirectoryList
+    {
+        $directoryListMock = $this->getMockBuilder(DirectoryList::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $directoryListMock->method('getRoot')->willReturn('/var/www/html');
+        $directoryListMock->method('getPath')->willReturn('/var/www/html/pub/media');
+
+        return $directoryListMock;
+    }
+}
