c2022a22f367659d0022189afe7001486057ae1f
Jisse Reitsma <jisse@yireo.com>
2022-07-04 11:44:48 +0200

    Cleanup comments

--- .github/workflows/extdn-integration-tests-pre-install-m23.sh
+++ .github/workflows/extdn-integration-tests-pre-install-m23.sh
@@ -7,8 +7,3 @@ composer require yireo/magento2-replace-bundled:^3.0 --no-update
 composer require yireo/magento2-replace-inventory:^4.0 --no-update
 composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
 
-#composer config --no-plugins allow-plugins.laminas/laminas-dependency-plugin true
-#composer config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
-#composer config --no-plugins allow-plugins.magento/composer-root-update-plugin true
-#composer config --no-plugins allow-plugins.magento/inventory-composer-installer true
-#composer config --no-plugins allow-plugins.magento/magento-composer-installer true
