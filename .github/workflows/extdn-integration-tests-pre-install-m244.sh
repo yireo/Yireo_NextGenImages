69efdec0f19677138087a5eaaf2d71a443d80feb
jisse Reitsma <jisse@yireo.com>
2022-07-04 08:26:10 +0200

    Comment out all composer plugins as a test for GitHub Actions

--- .github/workflows/extdn-integration-tests-pre-install-m244.sh
+++ .github/workflows/extdn-integration-tests-pre-install-m244.sh
@@ -10,9 +10,9 @@ composer require yireo/magento2-replace-bundled:^4.0 --no-update
 composer require yireo/magento2-replace-inventory:^4.0 --no-update
 composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
 
-composer config --no-plugins allow-plugins.laminas/laminas-dependency-plugin true
-composer config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
-composer config --no-plugins allow-plugins.magento/composer-root-update-plugin true
-composer config --no-plugins allow-plugins.magento/inventory-composer-installer true
-composer config --no-plugins allow-plugins.magento/magento-composer-installer true
+#composer config --no-plugins allow-plugins.laminas/laminas-dependency-plugin true
+#composer config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
+#composer config --no-plugins allow-plugins.magento/composer-root-update-plugin true
+#composer config --no-plugins allow-plugins.magento/inventory-composer-installer true
+#composer config --no-plugins allow-plugins.magento/magento-composer-installer true
 
