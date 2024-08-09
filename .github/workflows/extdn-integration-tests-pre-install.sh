6b7cee37029b74f2bc7218233babc7b6ef4ff4e2
Jisse Reitsma <jisse@yireo.com>
2024-08-09 13:32:52 +0200

    Adjust pipelines

--- .github/workflows/extdn-integration-tests-pre-install.sh
+++ .github/workflows/extdn-integration-tests-pre-install.sh
@@ -3,9 +3,7 @@ composer config minimum-stability dev
 composer config prefer-stable false
 
 composer require yireo/magento2-integration-test-helper --no-update
-composer require monolog/monolog:2.6.0 --no-update
 
-composer require yireo/magento2-integration-test-helper --no-update
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
 composer require yireo/magento2-replace-inventory:^4.0 --no-update
 composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
--- .github/workflows/extdn-phpstan-tests-pre-install.sh
+++ .github/workflows/extdn-phpstan-tests-pre-install.sh
@@ -6,4 +6,3 @@ composer require yireo/magento2-integration-test-helper --no-update
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
 composer require yireo/magento2-replace-inventory:^4.0 --no-update
 composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
-
